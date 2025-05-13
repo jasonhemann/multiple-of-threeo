7847306c36594856fc0b79635f5000c063d4c794
Jason Hemann <jason.hemann@gmail.com>
2025-05-13 11:24:19 -0400

    Commenting and improving old code for mult-of-3

--- multiple-of-three.rkt
+++ multiple-of-three.rkt
@@ -1,14 +1,33 @@
-#lang htdp/asl
+#lang htdp/isl+
 (require 2htdp/abstraction)
+(require "void.rkt")
 
-"Turn crashing functions into succesful relations"
+"Turn crashing functions into successful relations"
 
-;; An alternative would be to use #<void>, b/c you want some unit
-;; value, rather than a boolean.
+;; An attempted approach using the *SL languages so that the miniKanren "turn the crank" would
+;; directly produce the 3 relations we are interested in rather than the style where you have an
+;; extra boolean flag in the arguments list. Each n-ary function becomes an n+1-ary relation, so
+;; to have an 1-ary relation seems to require a 0-arity function.
+;;
+;; Returning void is a way to reasonably treat your n-ary function like it wants to be an n-ary relation, b/c it has no "true return value".
+;; This is intended to avoid turning that function into an n+1 ary relation, like you would do when you have a real return value.
+;;
+;; Right now these tests are broken
+;;
+;; Related to work on "turn crashing functions into successful relations" draft in ~/Documents
+;;
+(define (evens#-odds#%3=0 bbn)
+  (match bbn
+    ['(1 1) (void)]
+    [(cons a (cons ad dd))
+     (cond
+       [(equal? a ad) (evens#-odds#%3=0 dd)]
+       [(equal? (list a ad) '(1 0)) (evens#-odds#%3=2 dd)]
+       [(equal? (list a ad) '(0 1)) (evens#-odds#%3=0 dd)])]))
 
 (define (evens#-odds#%3=1 bbn)
   (match bbn
-    ['(1) #t]
+    ['(1) (void)]
     [(cons a (cons ad dd))
      (cond
        [(equal? a ad) (evens#-odds#%3=1 dd)]
@@ -17,21 +36,12 @@
 
 (define (evens#-odds#%3=2 bbn)
   (match bbn
-    ['(0 1) #t]
+    ['(0 1) (void)]
     [(cons a (cons ad dd))
      (cond
        [(equal? a ad) (evens#-odds#%3=2 dd)]
        [(equal? (list a ad) '(1 0)) (evens#-odds#%3=1 dd)]
        [(equal? (list a ad) '(0 1)) (evens#-odds#%3=0 dd)])]))
-
-(define (evens#-odds#%3=0 bbn)
-  (match bbn
-    ['(1 1) #t]
-    [(cons a (cons ad dd))
-     (cond
-       [(equal? a ad) (evens#-odds#%3=0 dd)]
-       [(equal? (list a ad) '(1 0)) (evens#-odds#%3=2 dd)]
-       [(equal? (list a ad) '(0 1)) (evens#-odds#%3=1 dd)])]))
 
 (check-expect (evens#-odds#%3=0 '(1 1)) #t)
 
--- multiple-of-threeo.rkt
+++ multiple-of-threeo.rkt
@@ -10,8 +10,25 @@
 ;;
 ;; Relies on two help relations that track other remainders.
 ;;
+;; Abstraction over the concrete relations via macro
 
-(defrel (same-counto bn)
+
+(define (make-operator base-n self one-up one-down)
+  (λ (bn)
+	(conde
+	  [(== bn base-n)]
+	  [(fresh (a ad dd)
+		 (== `(,a ,ad . ,dd) bn)
+		 (conde
+		   [(== a ad) (self dd)]
+		   [(== `(,a ,ad) '(1 0)) (one-up dd)]
+		   [(== `(,a ,ad) '(0 1)) (one-down dd)]))])))
+
+(define mod+2o      (lambda (bn) ((make-operator '(1) mod+2o same-counto mod+1o) bn)))
+(define mod+1o      (lambda (bn) ((make-operator '(0 1) mod+1o mod+2o same-counto) bn)))
+(define same-counto (lambda (bn) ((make-operator '(1 1) same-counto mod+1o mod+2o) bn)))
+
+#;(defrel (same-counto bn)
   (conde
     [(== bn `(1 1))]
     [(fresh (a ad dd)
@@ -21,7 +38,7 @@
          [(== `(,a ,ad) '(1 0)) (mod+1o dd)]
          [(== `(,a ,ad) '(0 1)) (mod+2o dd)]))]))
 
-(defrel (mod+1o bn)
+#;(defrel (mod+1o bn)
   (conde
     [(== bn `(0 1))]
     [(fresh (a ad dd)
@@ -31,7 +48,7 @@
          [(== `(,a ,ad) '(1 0)) (mod+2o dd)]
          [(== `(,a ,ad) '(0 1)) (same-counto dd)]))]))
 
-(defrel (mod+2o bn)
+#;(defrel (mod+2o bn)
   (conde
     [(== bn '(1))]
     [(fresh (a ad dd)
@@ -56,13 +73,13 @@ cpu time: 86 real time: 88 gc time: 15
 
 (check-equal?
   (run 10 (q) (same-counto q))
-  '((1 1)
-    (_.0 _.0 1 1)
-    (1 0 0 1)
-    (0 1 1)
-    (_.0 _.0 _.1 _.1 1 1)
-    (_.0 _.0 1 0 0 1)
-    (_.0 _.0 0 1 1)
-    (1 0 _.0 _.0 0 1)
-    (0 1 _.0 _.0 1)
-    (_.0 _.0 _.1 _.1 _.2 _.2 1 1)))
+  '((1 1)                  ;; 3 
+    (_.0 _.0 1 1)          ;; 12 v 15
+    (1 0 0 1)              ;; 9
+    (0 1 1)                ;; 6
+    (_.0 _.0 _.1 _.1 1 1)  ;; 96 (+ 3 v 0) (+ 12 v 0)
+    (_.0 _.0 1 0 0 1)      ;; 36 + (3 v 0)
+    (_.0 _.0 0 1 1)        ;; 24 + (3 v 0)
+    (1 0 _.0 _.0 0 1)      ;; 33 + (12 v 0)
+    (0 1 _.0 _.0 1)        ;; 18 + (12 v 0)
+    (_.0 _.0 _.1 _.1 _.2 _.2 1 1))) ;; 192 + (3 v 0) + (12 v 0) + (48 v 0)
